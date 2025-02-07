3f19f2ed149a46004d9234f2d96f8fa3c3d12da3
24csu067 <24csu067@ncuindia.edu>
2025-02-07 20:22:24 +0530

    Add files via upload

---
+++ q4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int n,x;
+    cout<<"Enter size of matrix\n";
+    cin>>n;
+     for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            x=n*i+j+1;
+            cout<<left<<setw(5)<<x<<"";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
